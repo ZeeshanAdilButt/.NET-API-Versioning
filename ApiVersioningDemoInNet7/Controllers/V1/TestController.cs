af4fa1540df622699d896bbf93ef16121ab3b63d
Zeeshan Adil <zeeAButt@outlook.com>
2024-03-18 16:32:06 +0500

    added api versioning

---
+++ ApiVersioningDemoInNet7/Controllers/V1/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVersioningDemoInNet7.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiVersion(1.0)]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet, Route("testmethod")]
+        public string TestMethod()
+        {
+            return "Test method call successful";
+        }
+
+        //[ApiVersion(2.0)]
+        //[HttpGet, Route("testmethod")]
+        //public string TestMethod2()
+        //{
+        //    return "Test method 22 call successful";
+        //}
+    }
+}
